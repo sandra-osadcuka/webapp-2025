1aa621a244ccaaef98b3b0628aff661f91dfb766
Mārtiņš Tararujs <martins.tararujs@testdevlab.com>
2025-10-05 19:49:44 +0300

    Add k8s templates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -18,7 +18,7 @@ function App() {
 
   return (
     <div>
-      <h1>Data from Backend - New Version V2</h1>
+      <h1>Data from Backend - New Version V3</h1>
       <ul>
         {data.map(item => (
           <li key={item.id}>{item.name}</li>
